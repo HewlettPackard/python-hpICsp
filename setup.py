bf6150aa80c5be4b3b610c4fad25255f1f22ebb7
Camila Balestrin <balestrin.camila@hotmail.com>
2017-04-18 16:07:32 -0300

    update setup.py to pushish at pypi

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@
 from distutils.core import setup
 
 setup(name='hpICsp',
-      version='1.0',
+      version='1.0.1',
       description='HP Insight Control Server Provisioning Python Library',
       url='https://github.com/HewlettPackard/python-hpICsp',
-      download_url="https://github.com/HewlettPackard/python-hpICsp/tarball/v1.0",
+      download_url="https://github.com/HewlettPackard/python-hpICsp/tarball/v1.0.1",
       author='Hewlett-Packard Development Company L.P.',
       author_email='oneview-pythonsdk@hpe.com',
       license='MIT',
